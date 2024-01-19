1c593338aa019e5af6be1959fd7216dd9785813f
Maarcbaz <138406453+Maarcbaz@users.noreply.github.com>
2024-01-19 16:06:28 +0100

    all done!

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let menu = document.querySelector('.open')
 let overlay = document.querySelector('.overlay')
 menu.addEventListener('click', () => {
-    if(overlay.style.width == '230px') {
+    if (overlay.style.width == '230px') {
         overlay.style.width = '0'
     }
     else {
@@ -22,3 +22,52 @@ function addActive(add) {
     document.getElementById(add) = currentTarget.classList.add('active')
     // add.currentTarget.classList.add('active')
 }
+let btnFt = document.getElementById('btnFooter')
+btnFt.addEventListener('suubmit', () => {
+    function validateForm() {
+        return false
+    }
+})
+validateForm()
+
+function validateForm() {
+    return false
+}
+
+function popNav() {
+    document.getElementById('popup_msg').classList.toggle('activePop')
+}
+
+
+// let inputs = document.querySelectorAll('input')
+// let textAreas = document.querySelectorAll('textarea')
+// textAreas.forEach((textArea) => {
+//     textArea.addEventListener('focus', function () {
+//         textArea.style.borderColor = 'white'
+//     })
+//     textArea.addEventListener('blur', function () {
+//         textArea.style.borderColor = ''
+//     })
+// })
+// inputs.forEach((input) => {
+//     input.addEventListener('focus', function () {
+//         input.style.borderColor = 'white'
+//     })
+//     input.addEventListener('blur', function () {
+//         input.style.backgroundColor = ''
+//     })
+// })
+
+
+
+// function navNavigation(evt) {
+//     let allNav = document.querySelectorAll('.allNav')
+//     let i;
+//     for (i = 0; i < allNav.lenght; i++) {
+//         allNav.className[i] = allNav.className[i].replace(" active", '')
+//     }
+//     if (allNav[i] === allNav.className == ('allNav')) {
+//         allNav.classList.remove(' active')
+//     }
+//     evt.currentTarget.className = ' active'
+// }
